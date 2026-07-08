aaea4de87e921d6251287e338a5085fed58b5aac
nikhilrana13 <nikhilrajput060@gmail.com>
2026-07-08 17:25:35 +0530

    feat: add reset functionality for all API caches in resetApiCache utility

--- frontend/src/utils/resetApiCache.js
+++ frontend/src/utils/resetApiCache.js
@@ -1,7 +1,13 @@
 import { DashboardApi } from "@/redux/api/DashboardApi";
+import { ExecutionApi } from "@/redux/api/ExecutionApi";
+import { WorkFlowApi } from "@/redux/api/WorkFlowApi";
+import { WorkSpaceApi } from "@/redux/api/WorkspaceApi";
 
 
 
 export const resetAllApiCaches = () => (dispatch) => {
   dispatch(DashboardApi.util.resetApiState());
+  dispatch(ExecutionApi.util.resetApiState())
+  dispatch(WorkFlowApi.util.resetApiState())
+  dispatch(WorkSpaceApi.util.resetApiState())
 };
