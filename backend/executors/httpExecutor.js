e66781e2e81cd2813a2eaae0a5fd0d5f403c3cc8
nikhilrana13 <nikhilrajput060@gmail.com>
2026-08-13 16:03:58 +0530

    fix: update import path for resolveValue utility in httpExecutor.js

--- backend/executors/httpExecutor.js
+++ backend/executors/httpExecutor.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { resolveValue } from "../utils/resolveValue";
+import { resolveValue } from "../utils/resolveValue.js";
 
 export const HttpExecutor = async (node, context) => {
   try {
